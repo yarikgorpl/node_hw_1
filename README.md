a9488d8fae525138ba72f1826c03ee8ea66c9fcf
Yaroslav Protsiuk <jaroslav.procjuk@gmail.com>
2023-09-13 14:58:14 +0300

    Зробив дз ,вперше відправляю на перевірку
    Доданий один метод на редагування контакту,якого не вимагало завдання.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node_hw_1
+
+https://ibb.co/xY3b8bg
+https://ibb.co/1zK5q82
+https://ibb.co/TcDYW9v
+https://ibb.co/JnTb9jx
---
+++ contacts.js
@@ -0,0 +1,35 @@
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./db");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "read":
+      const allContacts = await contacts.getAll();
+      return console.log(allContacts);
+    case "getById":
+      const oneContact = await contacts.getById(id);
+      return console.log(oneContact);
+    case "addContact":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case "updateById":
+      const updateContact = await contacts.updateById(id, {
+        name,
+        email,
+        phone,
+      });
+      return console.log(updateContact);
+    case "deleteById":
+      const deleteContact = await contacts.deleteById(id);
+      return console.log(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+console.log(argv);
+invokeAction(argv);
